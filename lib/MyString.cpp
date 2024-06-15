baaf3fcb5b6fe46af8053e5b7e032e7306c89a47
IliyanGavrilov <wardheromgrtoxic@gmail.com>
2024-06-15 12:57:38 +0300

    Add vector and string

---
+++ lib/MyString.cpp
@@ -0,0 +1,188 @@
+#include "MyString.hh"
+#include <cstring> // For strlen, strcpy, strcmp, etc.
+#include <limits>
+
+void MyString::reallocate(size_t size) {
+  this->cap = next_pow_of_2(size);
+  char *temp = new char[this->cap] {'\0'};
+
+  strcpy(temp, elems);
+
+  delete[] elems;
+  this->elems = temp;
+}
+
+size_t MyString::next_pow_of_2(size_t num) {
+  size_t pow = 1;
+
+  while(pow < num) {
+    pow *= 2;
+  }
+
+  return pow;
+}
+
+void MyString::free() {
+  this->cap = 0;
+  delete[] this->elems;
+  this->elems = nullptr;
+}
+
+void MyString::copyFrom(const MyString& other) {
+  reallocate(strlen(other.elems) + 1);
+  strcpy(elems, other.elems);
+}
+
+void MyString::moveFrom(MyString&& other) noexcept {
+  this->cap = other.cap;
+  this->elems = other.elems;
+
+  other.cap = 0;
+  other.elems = nullptr;
+}
+
+MyString::MyString(const MyString& other) {
+  copyFrom(other);
+}
+
+MyString::MyString(MyString&& other) noexcept {
+  moveFrom(std::move(other));
+}
+
+MyString::MyString(const char *str) {
+  if(!str) {
+    throw std::invalid_argument("Nullptr exception, creating string!");
+  }
+
+  reallocate(strlen(str) + 1);
+  strcpy(elems, str);
+}
+
+MyString::MyString(size_t size) {
+  reallocate(size + 1);
+}
+
+MyString::~MyString() {
+  free();
+}
+
+MyString& MyString::operator=(const MyString& other) {
+  if(this != &other) {
+    free();
+    copyFrom(other);
+  }
+
+  return *this;
+}
+
+MyString& MyString::operator=(MyString&& other) noexcept {
+  if(this != &other) {
+    free();
+    moveFrom(std::move(other));
+  }
+
+  return *this;
+}
+
+size_t MyString::length() const {
+  return strlen(elems);
+}
+
+size_t MyString::size() const {
+  return strlen(elems);
+}
+
+bool MyString::empty() const {
+  return strlen(elems) == 0;
+}
+
+const char* MyString::c_str() const {
+  return this->elems;
+}
+
+size_t MyString::capacity() const {
+  return cap;
+}
+
+size_t MyString::max_size() {
+  return std::numeric_limits<size_t>::max();
+}
+
+char *MyString::data() {
+  return this->elems;
+}
+
+const char& MyString::operator[](size_t index) const {
+  if(index >= strlen(elems)) {
+    throw std::out_of_range("MyString::operator[] const: Reading out of range!");
+  }
+
+  return this->elems[index];
+}
+
+char& MyString::operator[](size_t index) {
+  if(index >= length()) {
+    throw std::out_of_range("MyString::operator[] const: Reading out of range!");
+  }
+
+  return elems[index];
+}
+
+MyString& MyString::operator=(const char *str) {
+  return this->operator=(MyString(str));
+}
+
+bool MyString::operator==(const char *str) const {
+  return (strcmp(elems, str) == 0);
+}
+
+bool MyString::operator==(const MyString &s) const {
+  return (strcmp(elems, s.elems) == 0);
+}
+
+std::ostream& operator<<(std::ostream& os, const MyString& str) {
+  os << str.elems;
+
+  return os;
+}
+
+//std::istream& operator>>(std::istream& is, MyString& str) {
+//
+//}
+
+
+
+
+
+
+
+//MyString& MyString::operator+=(char c) {
+//  if(length() + 1 >)
+//}
+//
+//MyString& MyString::operator+=(const char *str) {
+//  return this->operator+=(MyString(str));
+//}
+//
+//MyString& MyString::operator+=(const MyString &other) {
+//    if(length() + other.length() > cap) {
+//      cap = next_pow_of_2(length() + other.length() + 1);
+//      reallocate(strlen(elems) + 1);
+//    }
+//
+//    size_t curr = strlen(elems) + 1;
+//    size_t other_curr = strlen(other.elems) + 1;
+//
+//    for(size_t i = curr; i <= curr + other_curr; i++) {
+//      elems[i] = other.elems[i - curr];
+//    }
+//}
+//
+//MyString operator+(const MyString& lhs, const MyString& rhs) {
+//  MyString result(lhs.length() + rhs.length());
+//
+//  result += lhs;
+//  result += rhs;
+//
+//  return result;
+//}
---
+++ lib/MyString.hh
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream> // size_t, std::move
+
+class MyString
+{
+  char* elems = new char[1] {'\0'};
+  size_t cap = 1; // capacity of allocated memory
+
+  void reallocate(size_t size);
+
+  static size_t next_pow_of_2(size_t num);
+
+  void free();
+  void copyFrom(const MyString& other);
+  void moveFrom(MyString&& other) noexcept;
+public:
+  MyString()=default;
+  MyString(const MyString& other);
+  MyString(MyString&& other) noexcept;
+  MyString(const char *str);
+  explicit MyString(size_t size);
+  ~MyString();
+
+  MyString& operator=(const MyString& other);
+  MyString& operator=(MyString&& other) noexcept;
+
+  size_t length() const;
+  size_t size() const;
+  bool empty() const;
+  size_t capacity() const;
+  static size_t max_size();
+  char *data();
+
+  //size_t find(const char s);
+  //size_t find(const char *s);
+  //size_t find(const MyString& s);
+
+  const char* c_str() const;
+
+  char& operator[](size_t index);
+  const char& operator[](size_t index) const;
+  MyString& operator=(const char *str);
+//  MyString& operator+=(char c);
+//  MyString& operator+=(const char *str);
+//  MyString& operator+=(const MyString &other);
+  bool operator==(const char *str) const;
+  bool operator==(const MyString &str) const;
+  friend std::ostream& operator<<(std::ostream& os, const MyString& str);
+//  friend std::istream& operator>>(std::istream& is, MyString& str);
+//  friend MyString operator+(const MyString& lhs, const MyString& rhs);
+};
+
+//bool operator==(const MyString& lhs, const MyString& rhs);
+//bool operator!=(const MyString& lhs, const MyString& rhs);
+//bool operator<(const MyString& lhs, const MyString& rhs);
+//bool operator<=(const MyString& lhs, const MyString& rhs);
+//bool operator>(const MyString& lhs, const MyString& rhs);
+//bool operator>=(const MyString& lhs, const MyString& rhs);
