565a692f667da775cae7cd6007cfbb10d33efe1f
IliyanGavrilov <wardheromgrtoxic@gmail.com>
2024-06-21 20:24:14 +0300

    Add messaging and fix id

--- EGN/ID.hh
+++ EGN/ID.hh
@@ -5,7 +5,7 @@
 #define MOD 11 // The checkSum number is reduced to mod11 (module 11)
 
 class ID {
-  MyString EGN;
+  char EGN[EGN_LEN + 1];
   IDRegion region;
 
   void setEGN(const MyString &str);
@@ -23,4 +23,5 @@ public:
   bool isGirl() const;
 
   friend bool operator==(const ID& id1, const ID& id2);
+  friend std::ostream& operator<<(std::ostream& os, const ID& egn);
 };
